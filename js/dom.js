e2b668e3da2dcceb2222976f773aa04ae52370c9
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-09-11 11:31:29 -0300

    DOM js, id tag edits & desktop layer creation

---
+++ js/dom.js
@@ -0,0 +1,13 @@
+//
+// DOM elements - Desktop
+
+const $desktopAll = document.querySelector("#desktopAll");
+
+// DOM elements - Taskbar
+
+const $startBtn = document.querySelector("#startBtn");
+const $notifArea = document.querySelector("#notifArea");
+
+// DOM elements - Notification Menu
+
+const $wallpaperBtn = document.querySelector("#wallpaperBtn");
