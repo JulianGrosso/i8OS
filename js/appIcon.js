f583f368f8e5da27755d0d2b7449f6e6e561ae67
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-09-12 18:00:55 -0300

    App window bug fix

--- js/appIcon.js
+++ js/appIcon.js
@@ -1,26 +1,8 @@
+let windowState = false;
+
 $appIcon01.addEventListener("click", () => {
-	let windowAppState = $appWindow.classList.value;
-
-	if (windowAppState === "app-windows hidden") {
-		$appWindow.classList.remove("hidden");
+	if (windowState === false) {
+		openApp();
+		windowState = true;
 	}
-
-	const appDeploy = document.createElement("div");
-	appDeploy.classList.add("app-deploy");
-	appDeploy.setAttribute("id", "appDeploy");
-
-	appDeploy.innerHTML = `APP EJECUTANDOSE`;
-
-	$appContainer.appendChild(appDeploy);
-});
-
-$closeWindow.addEventListener("click", () => {
-	let windowAppState = $appWindow.classList.value;
-
-	if (windowAppState === "app-windows") {
-		$appWindow.classList.add("hidden");
-	}
-
-	let appDeploy = document.getElementById("appDeploy");
-	appDeploy.remove();
 });
---
+++ js/appwindow.js
@@ -0,0 +1,33 @@
+function openApp() {
+	let windowAppState = $appWindow.classList.value;
+
+	if (windowAppState === "app-windows hidden") {
+		$appWindow.classList.remove("hidden");
+	}
+
+	const appDeploy = document.createElement("div");
+	appDeploy.classList.add("app-deploy");
+	appDeploy.setAttribute("id", "appDeploy");
+
+	appDeploy.innerHTML = `APP EJECUTANDOSE`;
+
+	$appContainer.appendChild(appDeploy);
+}
+
+function closeApp() {
+	let windowAppState = $appWindow.classList.value;
+
+	if (windowAppState === "app-windows") {
+		$appWindow.classList.add("hidden");
+	}
+
+	let appDeploy = document.getElementById("appDeploy");
+	appDeploy.remove();
+}
+
+$closeWindow.addEventListener("click", () => {
+	if (windowState === true) {
+		closeApp();
+		windowState = false;
+	}
+});
